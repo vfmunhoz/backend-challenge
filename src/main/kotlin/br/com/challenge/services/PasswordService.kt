873b14d17d5a7ca1cb1f7d82dd48695bbe85baf9
Vinicius Montes Munhoz <viniciusfmunhoz@gmail.com>
2020-05-06 22:32:26 -0300

    feat: add password contains digits validation

--- src/main/kotlin/br/com/challenge/services/PasswordService.kt
+++ src/main/kotlin/br/com/challenge/services/PasswordService.kt
@@ -10,12 +10,14 @@ class PasswordService {
         val validationErrorMessages = mutableListOf<String>()
         val (isLengthValid, lengthValidationMessage) = validateLength(password)
         val (isCaseValid, caseValidationMessage) = validatePasswordCase(password)
+        val (hasDigits, digitsValidationMessage) = validatePasswordHasDigits(password)
 
         if(lengthValidationMessage != null) validationErrorMessages.add(lengthValidationMessage)
         if(caseValidationMessage != null) validationErrorMessages.add(caseValidationMessage)
+        if(digitsValidationMessage != null) validationErrorMessages.add(digitsValidationMessage)
 
         return PasswordStatus(
-                isLengthValid && isCaseValid,
+                isLengthValid && isCaseValid && hasDigits,
                 validationErrorMessages.toList()
         )
     }
@@ -35,8 +37,17 @@ class PasswordService {
         }
     }
 
+    private fun validatePasswordHasDigits(password: String): Pair<Boolean, String?> {
+        return if(PASSWORD_HAS_DIGITS_REGEX.toRegex().matches(password)) {
+            true to null
+        } else {
+            false to "Password must have at least one digit"
+        }
+    }
+
     companion object {
         private const val PASSWORD_MINIMUM_SIZE = 9
         private const val PASSWORD_PATTERN_REGEX = "((?=.*[A-Z])(?=.*[a-z])).*"
+        private const val PASSWORD_HAS_DIGITS_REGEX = "(?=.*\\d).*"
     }
 }
--- src/test/kotlin/br/com/challenge/services/PasswordServiceTest.kt
+++ src/test/kotlin/br/com/challenge/services/PasswordServiceTest.kt
@@ -34,7 +34,7 @@ class PasswordServiceTest {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = ["abtp9!foo", "ABTP9!FOO" ])
+    @ValueSource(strings = ["abtp9!foo", "ABTP9!FOO"])
     fun `password must be invalid if it doesn't have at least 1 capital letter and 1 lower case letter`(password: String) {
         val (isValid, validationErrors) = passwordService.validatePassword(password)
 
@@ -43,8 +43,19 @@ class PasswordServiceTest {
         assertThat(validationErrors.contains(PASSWORD_LETTER_CASE_ERROR_MESSAGE)).isTrue()
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = ["AbTp(!foo", "Jhdg*hsh!"])
+    fun `password must be invalid if it doesn't have at least 1 digit`(password: String) {
+        val (isValid, validationErrors) = passwordService.validatePassword(password)
+
+        assertThat(isValid).isFalse()
+        assertThat(validationErrors.isEmpty()).isFalse()
+        assertThat(validationErrors.contains(PASSWORD_NO_DIGIT_ERROR_MESSAGE)).isTrue()
+    }
+
     companion object {
         private const val PASSWORD_LENGTH_ERROR_MESSAGE = "Password has less than 9 characters"
         private const val PASSWORD_LETTER_CASE_ERROR_MESSAGE = "Password must have at least one lower and one upper case letters"
+        private const val PASSWORD_NO_DIGIT_ERROR_MESSAGE = "Password must have at least one digit"
     }
 }
