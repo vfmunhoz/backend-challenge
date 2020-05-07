4c2cc186c508d358bd34ce68b6d2234aff3a0660
Vinicius Montes Munhoz <viniciusfmunhoz@gmail.com>
2020-05-07 03:54:48 -0300

    test: add validations for success api call

--- src/integrationTest/kotlin/br/com/challenge/PasswordValidationTest.kt
+++ src/integrationTest/kotlin/br/com/challenge/PasswordValidationTest.kt
@@ -1,10 +1,16 @@
 package br.com.challenge
 
 import assertk.assertThat
+import assertk.assertions.isEqualTo
 import assertk.assertions.isNotNull
+import assertk.assertions.isNullOrEmpty
+import assertk.assertions.isTrue
 import br.com.challenge.entities.PasswordValidationRequest
+import br.com.challenge.entities.PasswordValidationResponse
 import br.com.challenge.extensions.ApplicationExtension
+import io.micronaut.core.type.Argument
 import io.micronaut.http.HttpRequest
+import io.micronaut.http.HttpStatus
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.extension.RegisterExtension
@@ -17,10 +23,20 @@ class PasswordValidationTest {
     val application = ApplicationExtension()
 
     @Test
-    fun `return a 200 htpp status response calling a password validation with a valid password`() {
-        val request = HttpRequest.POST("/", PasswordValidationRequest("AbTp9!foo"))
-        val body = application.client().toBlocking().retrieve(request)
+    fun `return a 200 htpp status response calling password validation with a valid password`() {
+        val request = HttpRequest.POST(PASSWORD_VALIDATION_ENDPOINT, PasswordValidationRequest(VALID_PASSWORD))
+        val body = application.client().toBlocking().exchange(request, Argument.of(PasswordValidationResponse::class.java))
 
-        assertThat(body).isNotNull()
+        assertThat(body.status).isEqualTo(HttpStatus.OK)
+        assertThat(body.body()).isNotNull().given { passwordResponse ->
+            assertThat(passwordResponse.isValid).isTrue()
+            assertThat(passwordResponse.validationErrors).isNullOrEmpty()
+        }
+    }
+
+    companion object {
+
+        private const val PASSWORD_VALIDATION_ENDPOINT = "/"
+        private const val VALID_PASSWORD = "AbTp9!foo"
     }
 }
--- src/integrationTest/kotlin/br/com/challenge/entities/Password.kt
+++ src/integrationTest/kotlin/br/com/challenge/entities/Password.kt
@@ -3,3 +3,8 @@ package br.com.challenge.entities
 data class PasswordValidationRequest(
     val password: String
 )
+
+data class PasswordValidationResponse(
+    val isValid: Boolean,
+    val validationErrors: List<String>?
+)
