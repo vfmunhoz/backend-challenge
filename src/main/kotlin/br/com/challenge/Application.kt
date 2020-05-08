4cb0595bcdbbf95b0557bbdcb362438f36c6c494
Vinicius Montes Munhoz <viniciusfmunhoz@gmail.com>
2020-05-08 10:51:15 -0300

    docs: add swagger configuration

--- src/main/kotlin/br/com/challenge/Application.kt
+++ src/main/kotlin/br/com/challenge/Application.kt
@@ -1,7 +1,18 @@
 package br.com.challenge
 
 import io.micronaut.runtime.Micronaut
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Contact
+import io.swagger.v3.oas.annotations.info.Info
 
+@OpenAPIDefinition(
+    info = Info(
+        title = "Password validation service",
+        version = "1.0.0",
+        description = "password validation endpoint definitions",
+        contact = Contact(url = "http://password-validation.com.br", name = "Vinicius")
+    )
+)
 object Application {
 
     @JvmStatic
